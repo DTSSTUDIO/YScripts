f68dd5d655422cc41f1449cf225177daa22730b6
Yunus Emre <yedhrab@gmail.com>
2019-05-12 04:44:35 +0300

    Yapılacaklar eklendi

--- Python Script/Markdown/dinamiclink_generator.py
+++ Python Script/Markdown/dinamiclink_generator.py
@@ -5,81 +5,94 @@ import time
 
 FILE_PATH = r"/home/yemreak/Documents/YScripts/temp/Vscode.md"
 
-links = []
-filestr = ""
-with open(FILE_PATH, "r") as file:
-    delims = ['[', ']', '(', ')']
-    for line in file:
-        # Her ayıracın konum indeksi
-        indexes = [-1 for i in range(len(delims))]
+# TODO: Fonksiyonel yapıyı kullan o daha hızlı, ve senin sevdiğin türde :D
 
-        # Satırları satır indeksiyle okuma
-        index = 0
-        for char in line:
-            # Karakter '[' ise indeksini alma
-            if char == delims[0]:
-                indexes[0] = index
 
-                # Diğer indeksleri sıfırlama
-                for i in range(1, len(indexes)):
-                    indexes[i] = -1
+def remove_staticlinks() -> str:
+    DELIMS = ['[', ']', '(', ')']
 
-            # "[" karakteri bulunduysa diğer karakterleri arama
-            elif indexes[0] != -1:
-                # "]" karakteri ise indeksini alma
-                if char == delims[1]:
-                    indexes[1] = index
+    links = []
+    filestr = ""
 
-                # "]" karakteri alındıysa diğer karakterleri arama
-                elif indexes[1] != -1:
-                    # Karakter ')' ise ve ']' dan hemen sonra geliyorsa indeksini alma
-                    if char == delims[2] and index == indexes[1] + 1:
-                        indexes[2] = index
+    def init_indexes(indexlist: list) -> list:
+        return [-1 for i in range(len(DELIMS))]
 
-                    # '(' bulundusya diğer karakterleri arama
-                    elif indexes[2] != -1:
-                        # Karakter ")" ise indeksini alma ve ek işlemler
-                        if char == delims[3]:
-                            indexes[3] = index
+    with open(FILE_PATH, "r") as file:
 
-                            # Linkleri ayrıştırma ve gerekli dizilere ekleme
-                            linkstr = line[indexes[2] + 1:indexes[3]]
+        for line in file:
+            # Her ayıracın konum indeksi
+            delim_indexes = init_indexes()
 
-                            # Anchor linkleri atlama
-                            if linkstr[0] != "#":
-                                link_index = len(links)
-                                found = False
-                                for i, link in enumerate(links):
-                                    if link == linkstr:
-                                        link_index = i
-                                        found = True
-                                        break
+            # Satırları satır indeksiyle okuma
+            index = 0
+            for char in line:
+                # Karakter '[' ise indeksini alma
+                if char == DELIMS[0]:
+                    delim_indexes[0] = index
 
-                                # Link yoksa ekleme
-                                if not found:
-                                    links.append(linkstr)
+                    # Diğer indeksleri sıfırlama
+                    for i in range(1, len(delim_indexes)):
+                        delim_indexes[i] = -1
 
-                                # Parantezleri ve aradaki linki dinamikleştirme
-                                line = line.replace(
-                                    f"({linkstr})", f"[{link_index}]")
+                # "[" karakteri bulunduysa diğer karakterleri arama
+                elif delim_indexes[0] != -1:
+                    # "]" karakteri ise indeksini alma
+                    if char == DELIMS[1]:
+                        delim_indexes[1] = index
 
-                                # Satır değişikliğini indekse yansıtma
-                                index -= len(linkstr) - len(str(link_index))
+                    # "]" karakteri alındıysa diğer karakterleri arama
+                    elif delim_indexes[1] != -1:
+                        # Karakter ')' ise ve ']' dan hemen sonra geliyorsa indeksini alma
+                        if char == DELIMS[2] and index == delim_indexes[1] + 1:
+                            delim_indexes[2] = index
 
-                            # İndeksleri sıfırlama (yenisi için hazırlama)
-                            indexes = [-1 for i in range(len(delims))]
+                        # '(' bulundusya diğer karakterleri arama
+                        elif delim_indexes[2] != -1:
+                            # Karakter ")" ise indeksini alma ve ek işlemler
+                            if char == DELIMS[3]:
+                                delim_indexes[3] = index
 
-                        # Boşluk karakteri link yapısını bozduğundan yeni link için hazırlama
-                        elif char == ' ':
-                            indexes = [-1 for i in range(len(delims))]
+                                # Linkleri ayrıştırma ve gerekli dizilere ekleme
+                                linkstr = line[delim_indexes[2] +
+                                               1:delim_indexes[3]]
 
-            # Yeni karaktere geçmeden önce indeksi ayarlama
-            index += 1
-        filestr += line
+                                # Anchor linkleri atlama
+                                if linkstr[0] != "#":
+                                    link_index = len(links)
+                                    found = False
+                                    for i, link in enumerate(links):
+                                        if link == linkstr:
+                                            link_index = i
+                                            found = True
+                                            break
 
-filestr += "\n"
-for i, link in enumerate(links):
-    filestr += f"\n[{i}]: {link}"
+                                    # Link yoksa ekleme
+                                    if not found:
+                                        links.append(linkstr)
+
+                                    # Parantezleri ve aradaki linki dinamikleştirme
+                                    line = line.replace(
+                                        f"({linkstr})", f"[{link_index}]")
+
+                                    # Satır değişikliğini indekse yansıtma
+                                    index -= len(linkstr) - \
+                                        len(str(link_index))
+
+                                # İndeksleri sıfırlama (yenisi için hazırlama)
+                                delim_indexes = init_indexes()
+
+                            # Boşluk karakteri link yapısını bozduğundan yeni link için hazırlama
+                            elif char == ' ':
+                                delim_indexes = init_indexes()
+
+                # Yeni karaktere geçmeden önce indeksi ayarlama
+                index += 1
+            filestr += line
+
+    filestr += "\n"
+    for i, link in enumerate(links):
+        filestr += f"\n[{i}]: {link}"
+
 
 with open(FILE_PATH, "w") as file:
     file.write(filestr)
