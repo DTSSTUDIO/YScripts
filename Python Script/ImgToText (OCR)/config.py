0731b8e806573d68a77fbac1a4a5020ef9933d64
Yunus Emre <yedhrab@gmail.com>
2019-06-12 19:05:06 +0300

    Python yeni scripitler eklendi 🚀

---
+++ Python Script/ImgToText (OCR)/config.py
@@ -0,0 +1,10 @@
+# Yapılandırma Ayarları
+
+BASE_DATA_DIR = "data"
+BASE_TEST_DIR = "test"
+
+DEFAULT_LANGUAGE = "tur"
+DEFAULT_BG = "cloudly-black-air.jpg"
+DEFAULT_FONT = "arial.ttf"
+DEFAULT_FONT_SIZE = 21
+DEFAULT_TEXT_START = (207, 37)
---
+++ Python Script/Markdown/html_to_pdf.py
@@ -0,0 +1,15 @@
+import pdfkit
+import os
+
+# https://github.com/wkhtmltopdf/wkhtmltopdf/releases
+path_wkthmltopdf = r'C:\Program Files\wkhtmltopdf\bin\wkhtmltopdf.exe'
+config = pdfkit.configuration(wkhtmltopdf=path_wkthmltopdf)
+
+for name in os.listdir():
+    if ".html" in name:
+        output = os.path.join("out",name.replace('.html', ''))
+        pdfkit.from_file(f"{name}", f"{output}.pdf", configuration=config)
+
+
+
+
