47c39960efef596763874beb7c6400465049b705
Yunus Emre <49655146+yedhrab@users.noreply.github.com>
2019-07-21 20:34:20 +0300

    HTML to PDF

--- Python Script/Markdown/html_to_pdf.py
+++ Python Script/Markdown/html_to_pdf.py
@@ -1,3 +1,4 @@
+import argparse
 import pdfkit
 import os
 
@@ -5,7 +6,26 @@ import os
 path_wkthmltopdf = r'C:\Program Files\wkhtmltopdf\bin\wkhtmltopdf.exe'
 config = pdfkit.configuration(wkhtmltopdf=path_wkthmltopdf)
 
-for name in os.listdir():
+parser = argparse.ArgumentParser(description='HTML to PDF.')
+parser.add_argument(
+        '--input-dir',
+        '-i',
+        dest="inputDir",
+        default=os.getcwd(),
+        help='Path of html items',
+        type=str,
+        )
+parser.add_argument(
+        '--output',
+        "-o",
+        dest="outDir",
+        default="out",
+        help='Clear the log file on startup.Default is No',
+        )
+
+args = parser.parse_args()
+for name in os.listdir(args.inputDir):
+    name = os.path.join(args.inputDir, name)
     if ".html" in name:
-        output = os.path.join("out", name.replace('.html', ''))
+        output = os.path.join(args.outDir, name.replace('.html', ''))
         pdfkit.from_file(f"{name}", f"{output}.pdf", configuration=config)
