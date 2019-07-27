ff0aaa5c21e3ec0ac68cb47b073f29b5bb838d24
Yunus Emre <49655146+yedhrab@users.noreply.github.com>
2019-07-27 16:54:28 +0300

    Önemsiz güncelleme

--- Javascript API/Nodejs/https_request.js
+++ Javascript API/Nodejs/https_request.js
@@ -1,5 +1,4 @@
-var https = require('https');
-
+var https = require("https");
 
 /**
  * HOW TO Make an HTTP Call - POST
@@ -7,47 +6,48 @@ var https = require('https');
 // do a POST request
 // create the JSON object
 jsonObject = JSON.stringify({
-    "nick": "uurtucrume9768",
-    "password": "yunus123"
+  nick: "uurtucrume9768",
+  password: "yunus123"
 });
 
 // prepare the header
 var postheaders = {
-    'Content-Type' : 'application/json',
-    'Content-Length' : Buffer.byteLength(jsonObject, 'utf8')
+  "Content-Type": "application/json",
+  "Content-Length": Buffer.byteLength(jsonObject, "utf8")
 };
 
-https://dev.gittigidiyor.com:8443/listingapi/rlws/anonymous/developer?method=registerDeveloper&outputCT=json&inputCT=json&lang=tr
+//dev.gittigidiyor.com:8443/listingapi/rlws/anonymous/developer?method=registerDeveloper&outputCT=json&inputCT=json&lang=tr
 
-// the post options
+https: // the post options
 var optionspost = {
-    host : 'dev.gittigidiyor.com',
-    port : 8443,
-    path : '/listingapi/rlws/anonymous/developer?method=registerDeveloper&outputCT=json&inputCT=json&lang=tr',
-    method : 'POST',
-    headers : postheaders
+  host: "dev.gittigidiyor.com",
+  port: 8443,
+  path:
+    "/listingapi/rlws/anonymous/developer?method=registerDeveloper&outputCT=json&inputCT=json&lang=tr",
+  method: "POST",
+  headers: postheaders
 };
 
-console.info('Options prepared:');
+console.info("Options prepared:");
 console.info(optionspost);
-console.info('Do the POST call');
+console.info("Do the POST call");
 
 // do the POST call
 var reqPost = https.request(optionspost, function(res) {
-    console.log("statusCode: ", res.statusCode);
-    // uncomment it for header details
-//  console.log("headers: ", res.headers);
+  console.log("statusCode: ", res.statusCode);
+  // uncomment it for header details
+  //  console.log("headers: ", res.headers);
 
-    res.on('data', function(d) {
-        console.info('POST result:\n');
-        process.stdout.write(d);
-        console.info('\n\nPOST completed');
-    });
+  res.on("data", function(d) {
+    console.info("POST result:\n");
+    process.stdout.write(d);
+    console.info("\n\nPOST completed");
+  });
 });
 
 // write the json data
 reqPost.write(jsonObject);
 reqPost.end();
-reqPost.on('error', function(e) {
-    console.error(e);
+reqPost.on("error", function(e) {
+  console.error(e);
 });
