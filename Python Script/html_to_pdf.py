63ae3213b48cb8da435f236a6b6f628b4972f1d1
Yunus Emre <49655146+yedhrab@users.noreply.github.com>
2019-08-08 12:36:15 +0300

    Modüller için ek açıklama eklendi

--- Python Script/html_to_pdf.py
+++ Python Script/html_to_pdf.py
@@ -1,39 +1,44 @@
 import argparse
-import pdfkit
 import os
-import colorama
-from colorama import Fore, Style
-from colorama import init
+
+try:
+    import colorama
+    import pdfkit
+    from colorama import Fore, Style, init
+except ModuleNotFoundError as e:
+    print(f"Gerekli modülleri yükleyin: 'python -m pip install {e.name}'")
+
 init(autoreset=True)
 
 # https://github.com/wkhtmltopdf/wkhtmltopdf/releases
+# choco install wkhtmltopdf
 path_wkthmltopdf = r'C:\Program Files\wkhtmltopdf\bin\wkhtmltopdf.exe'
 config = pdfkit.configuration(wkhtmltopdf=path_wkthmltopdf)
 css = r"C:\Users\Yedhrab\Desktop\workspace\markdown-pdf.css"
 
 parser = argparse.ArgumentParser(description='HTML to PDF.')
 parser.add_argument(
-        '--input-dir',
-        '-i',
-        dest="inputDir",
-        default=os.getcwd(),
-        help='Path of html items',
-        type=str,
-        )
+    '--input-dir',
+    '-i',
+    dest="inputDir",
+    default=os.getcwd(),
+    help='Path of html items',
+    type=str,
+)
 parser.add_argument(
-        '--output',
-        "-o",
-        dest="outDir",
-        default="out",
-        help='Pdf outputs destionation (default "out")',
-        )
+    '--output',
+    "-o",
+    dest="outDir",
+    default="out",
+    help='Pdf outputs destionation (default "out")',
+)
 parser.add_argument(
-        '--css',
-        "-c",
-        dest="css",
-        default=None,
-        help='Custom css style for PDF (default None)',
-        )
+    '--css',
+    "-c",
+    dest="css",
+    default=None,
+    help='Custom css style for PDF (default None)',
+)
 
 args = parser.parse_args()
 
@@ -50,11 +55,11 @@ for name in os.listdir(args.inputDir):
 
         try:
             if args.css is None:
-                pdfkit.from_file(f"{_input}", f"{_output}.pdf", configuration=config)
+                pdfkit.from_file(
+                    f"{_input}", f"{_output}.pdf", configuration=config)
             else:
-                pdfkit.from_file(f"{_input}", f"{_output}.pdf", configuration=config, css=css)
+                pdfkit.from_file(f"{_input}", f"{_output}.pdf",
+                                 configuration=config, css=css)
             print(Fore.GREEN + "Succes!")
         except Exception as e:
-            print(Fore.YELLOW + f"{e.args[-1].split()[-1]} while converting.")
-
-        
+            print(Fore.YELLOW + f"Error while converting.")
